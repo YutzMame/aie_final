4311a5d8aef2789322770c0256b944379f940e47
YutzMame <yutsu.ryo@gmail.com>
2025-06-17 02:24:23 +0000

    Add test.txt

---
+++ tests/unit/test_aie_final_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from aie_final.aie_final_stack import AieFinalStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in aie_final/aie_final_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = AieFinalStack(app, "aie-final")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
