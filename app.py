7fe6d85962137f2990fcafd863de04f24b3ebe97
YutzMame <yutsu.ryo@gmail.com>
2025-06-17 07:18:30 +0000

    バックエンド環境の構築

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from qa_system.qa_system_stack import AieFinalStack
+from qa_system.qa_system_stack import QaSystemStack
 
 
 app = cdk.App()
-AieFinalStack(app, "AieFinalStack",
+QaSystemStack(app, "QaSystemStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
